85584014c9a010cfc4a3ad7df61327b043760213
Raquel Leão <vc.raquel@gmail.com>
2021-09-23 17:42:40 -0300

    herança lista exercicio classes - classe Fornecedor - método obterSaldo

--- src/br/com/zup/herancaListaExercicioClasses/Main.java
+++ src/br/com/zup/herancaListaExercicioClasses/Main.java
@@ -2,6 +2,7 @@ package br.com.zup.herancaListaExercicioClasses;
 
 public class Main {
     public static void main(String[] args) {
-
+    Fornecedor fornecedor1 = new Fornecedor("Caio","rua4, bairro 5, cidade 6","123456789", 10000,4550.54);
+        System.out.println("Saldo do fornecedor: R$" + fornecedor1.obterSaldo());
     }
 }
