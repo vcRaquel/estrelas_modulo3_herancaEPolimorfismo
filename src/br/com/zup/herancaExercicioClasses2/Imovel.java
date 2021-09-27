ca8c1ee530c4e58a2f7797cddd3c855833f74e30
Raquel Leão <vc.raquel@gmail.com>
2021-09-27 10:52:30 -0300

    modelagem de classes com herança exercicio2 método para inserir um morador na lista de moradores

--- src/br/com/zup/herancaExercicioClasses2/Imovel.java
+++ src/br/com/zup/herancaExercicioClasses2/Imovel.java
@@ -8,5 +8,67 @@ public class Imovel {
     private String endereco;
     private double aluguel;
     private String funcionarioResponsavel;
-    List <Morador> moradores = new ArrayList<>();
+    List<Morador> moradores = new ArrayList<>();
+
+    //Métodos construtores
+
+    public Imovel() {
+
+    }
+
+    public Imovel(String identificador, String endereco, double aluguel, String funcionarioResponsavel) {
+        this.identificador = identificador;
+        this.endereco = endereco;
+        this.aluguel = aluguel;
+        this.funcionarioResponsavel = funcionarioResponsavel;
+    }
+
+    //Getters e Setters
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public double getAluguel() {
+        return aluguel;
+    }
+
+    public void setAluguel(double aluguel) {
+        this.aluguel = aluguel;
+    }
+
+    public String getFuncionarioResponsavel() {
+        return funcionarioResponsavel;
+    }
+
+    public void setFuncionarioResponsavel(String funcionarioResponsavel) {
+        this.funcionarioResponsavel = funcionarioResponsavel;
+    }
+
+    public List<Morador> getMoradores() {
+        return moradores;
+    }
+
+    public void setMoradores(List<Morador> moradores) {
+        this.moradores = moradores;
+    }
+
+    //Métodos
+    //Para inserir um morador na lista de moradores
+    public void inserirMorador(Morador novoMorador) {
+        moradores.add(novoMorador);
+    }
+    //exibindo valor do aluguel, endereço, moradores, funcionário responsável
 }
