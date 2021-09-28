6de19f0f0af6214e417fdd217dab4cec577b45a2
Raquel Leão <vc.raquel@gmail.com>
2021-09-28 17:35:07 -0300

    classe Sistema - Método para entrada de dados

--- src/br/com/zup/gestaoDeRestaurante/Sistema.java
+++ src/br/com/zup/gestaoDeRestaurante/Sistema.java
@@ -3,5 +3,9 @@ package br.com.zup.gestaoDeRestaurante;
 import java.util.Scanner;
 
 public class Sistema {
-
+    //método para entrada de dados
+    private static Scanner capturarDados(String mensagem) {
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
 }
